0dbcd89d2f67493995df252b6d8e0c4ea98a274c
Dipansu Munjal <munjaldipanshu@gmail.com>
2025-03-05 03:57:01 +0530

    add q1 assignment

---
+++ src/main/java/binarysearch/assignment/SortedInsertPosition.java
@@ -0,0 +1,17 @@
+package binarysearch.assignment;
+
+public class SortedInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (target < nums[mid]) hi = mid - 1;
+            else if (target > nums[mid]) lo = mid + 1;
+            else return mid;
+        }
+
+        return lo;
+    }
+}
